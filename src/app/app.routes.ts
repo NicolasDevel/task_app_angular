ed4f73a6ee49b4f4be2009e7344abaaa250e35db
NicoDev <nicolassabogal85@gmail.com>
2025-06-17 21:33:07 -0500

    repo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Tasks } from './pages/tasks/tasks';
+import { Profile } from './pages/profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: 'tasks',
+        component: Tasks
+    },
+    {
+        path: 'profile',
+        component: Profile
+    }
+];
---
+++ src/app/pages/tasks/tasks.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
+import { Task } from '../../interfaces/task';
+
+@Component({
+  selector: 'app-tasks',
+  imports: [ CommonModule, FormsModule ],
+  templateUrl: './tasks.html',
+  styleUrl: './tasks.css'
+})
+
+export class Tasks {
+  tasks: Task[] = [];
+  newTaskDescription: string = '';
+  errorMessage: string = '';
+
+  addTask(): void {
+    //Validar si la descripción está vacía
+    if(this.newTaskDescription.trim()){
+      //Crear una nueva tarea
+      const newTask: Task = {
+        id: this.tasks.length > 0 ? Math.max(... this.tasks.map(t => t.id)) + 1 : 1,
+        description: this.newTaskDescription,
+        completed: false
+      }
+      this.tasks.push(newTask)
+      this.newTaskDescription = ''
+      this.errorMessage = ''
+      this.saveTasks()
+    }else{
+      this.errorMessage = 'La descripción de la tarea no puede estar vacía' 
+    }
+  }
+
+  saveTasks():void {
+    localStorage.setItem('angular-tasks', JSON.stringify(this.tasks))
+  }
+}
